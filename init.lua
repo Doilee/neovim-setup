ce05697c28b4da6f1588cd2afa8d04d9048252d5
matthijsdam <matthijs.dam@amsterdam.nl>
2023-03-30 18:51:48 +0200

    First commit

---
+++ init.lua
@@ -0,0 +1,14 @@
+require("matthijs.remap")
+
+--vim.cmd 'colorscheme darcula-solid'
+--vim.cmd 'set termguicolors'
+
+-- use 4 spaces instead of tab (to replace existing tab use :retab)
+-- copy indent from current line when starting a new line
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.autoindent = true
+
+vim.opt.incsearch = true
