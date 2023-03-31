94e3dea09d1b5d32f22501e8792cf2417d3118ba
matthijsdam <matthijs0894@hotmail.com>
2023-03-31 13:10:29 +0200

    Setup LSP

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,23 +1,46 @@
-local lsp = require('lsp-zero').preset({})
+local lsp = require('lsp-zero')
+
+lsp.preset("recommended")
 
 lsp.ensure_installed({
     'phpactor',
+    'tsserver',
     'rust_analyzer',
-    'lua_ls'
 })
 
-lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({buffer = bufnr})
-end)
+
+-- Fix Undefined global 'vim'
+lsp.configure('lua-language-server', {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
+    }
+})
+
+--lsp.on_attach(function(client, bufnr)
+--  lsp.default_keymaps({buffer = bufnr})
+--end)
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
-    ['<CR>'] = cmp.mapping.confirm({select = true}),
+  ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  --["<C-Space>"] = cmp.mapping.complete(),
 })
 
+--cmp_mappings['C-y'] = nil
+
+lsp.setup_nvim_cmp({
+  mapping = cmp_mappings
+})
 
 -- (Optional) Configure lua language server for neovim
-require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+--require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
 lsp.setup()
+
+vim.diagnostic.config({
+    virtual_text = true
+})
